fde80399fd0df43e9f67d56ab028fcaa7d11bb05
Bob Kraemer <bob.kraemer.android@gmail.com>
2023-12-17 11:19:02 -0500

    Section 17 "Databases & 'Room'" code complete through "Room - The Database Class (and a first look at the generated code)"

---
+++ app/src/main/java/com/kraemericaindustries/getitdone/Task.kt
@@ -0,0 +1,14 @@
+package com.kraemericaindustries.getitdone
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity  //  a.k.a, a table
+data class Task(
+
+    @PrimaryKey(autoGenerate = true) val taskId: Int = 0,
+    val title: String,
+    val description: String? = null,
+    val isStarred: Boolean = false
+
+)
