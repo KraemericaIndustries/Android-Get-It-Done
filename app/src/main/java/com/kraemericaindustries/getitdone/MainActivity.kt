9f35e009d1ca1411c2bfbe2f2d098bf789595677
Bob Kraemer <bob.kraemer.android@gmail.com>
2023-12-17 07:09:01 -0500

    Section 17 "Databases & 'Room'" code complete through "Setting up the Tasks Fragment and ViewPager"

--- app/src/main/java/com/kraemericaindustries/getitdone/MainActivity.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/MainActivity.kt
@@ -2,10 +2,34 @@ package com.kraemericaindustries.getitdone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.kraemericaindustries.getitdone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = PagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = "Tasks"
+        }.attach()
+
+    }
+
+
+    inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount() = 1
+
+        override fun createFragment(position: Int): Fragment {
+            return TasksFragment()
+        }
     }
 }
