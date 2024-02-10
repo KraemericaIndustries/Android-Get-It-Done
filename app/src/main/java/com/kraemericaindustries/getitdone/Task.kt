72066b2ec6ab4dc1b744f37dea50896a18794f4c
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-02-10 12:16:35 -0500

    Verified code working on Hedgehog through lecture 236: "*Kotlin Foundations* - Named Arguments"

--- app/src/main/java/com/kraemericaindustries/getitdone/Task.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/Task.kt
@@ -5,10 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity  //  a.k.a, a table
 data class Task(
-
     @PrimaryKey(autoGenerate = true) val taskId: Int = 0,
     val title: String,
     val description: String? = null,
     val isStarred: Boolean = false
-
 )
