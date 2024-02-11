e777b6cd1c405ffd08de0b412072e078d0df855a
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-02-11 10:23:45 -0500

    Verified code working on Hedgehog through lecture 236: "*Kotlin Foundations* - Named Arguments"

--- app/src/main/java/com/kraemericaindustries/getitdone/data/GetItDoneDatabase.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/data/GetItDoneDatabase.kt
@@ -1,9 +1,11 @@
-package com.kraemericaindustries.getitdone
+package com.kraemericaindustries.getitdone.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.kraemericaindustries.getitdone.data.Task
 
 @Database(entities = [Task::class], version = 1)
 abstract class GetItDoneDatabase : RoomDatabase() {
 
+    abstract fun getTaskDao() : TaskDao
 }
