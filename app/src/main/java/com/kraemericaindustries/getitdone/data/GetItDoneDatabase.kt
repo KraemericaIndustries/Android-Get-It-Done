2f8e3f8831bfdc046efc33aa608c3f9605a7a490
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-28 08:34:08 -0400

    Completed code through lecture 256: "Making the Database a Singleton"

--- app/src/main/java/com/kraemericaindustries/getitdone/data/GetItDoneDatabase.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/data/GetItDoneDatabase.kt
@@ -11,12 +11,21 @@ abstract class GetItDoneDatabase : RoomDatabase() {
     abstract fun getTaskDao() : TaskDao
 
     companion object {
-        fun createDatabase(context: Context): GetItDoneDatabase {
-            return Room.databaseBuilder(
-                context,
-                GetItDoneDatabase::class.java,
-                "get-it-done-database"
-            ).build()
+
+        @Volatile
+        private var DATABASE_INSTANCE: GetItDoneDatabase? = null
+
+        fun getDatabase(context: Context): GetItDoneDatabase {
+
+            return DATABASE_INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    GetItDoneDatabase::class.java,
+                    "get-it-done-database"
+                ).build()
+                DATABASE_INSTANCE = instance
+                instance
+            }
         }
     }
 }
