9826879bf69acc3a537f5ee9af9d37e15c40d8b3
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-29 09:52:57 -0400

    Verified code working with DEV profile through lectures 261: "Handling RecyclerView Interactions, & Checkbox Listeners"

--- app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksAdapter.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksAdapter.kt
@@ -3,10 +3,12 @@ package com.kraemericaindustries.getitdone.ui.tasks
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.checkbox.MaterialCheckBox
 import com.kraemericaindustries.getitdone.data.Task
 import com.kraemericaindustries.getitdone.databinding.ItemTaskBinding
 
-class TasksAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
+class TasksAdapter(private val tasks: List<Task>, private val listener: TaskUpdatedListener) :
+    RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
 
     override fun getItemCount() = tasks.size
 
@@ -19,12 +21,31 @@ class TasksAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TasksAd
         holder.bind(tasks[position])
     }
 
-
-    inner class ViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(task: Task) {
             binding.textViewTitle.text = task.title
             binding.textViewDetails.text = task.description
+            binding.checkBox.addOnCheckedStateChangedListener { _, state ->
+                val updatedTask = when (state) {
+                    MaterialCheckBox.STATE_CHECKED -> task.copy(isComplete = true)
+                    else -> task.copy(isComplete = false)
+                }
+                listener.onTaskUpdated(updatedTask)
+            }
+            binding.toggleStar.addOnCheckedStateChangedListener { _, state ->
+                val updatedTask = when (state) {
+                    MaterialCheckBox.STATE_CHECKED -> task.copy(isStarred = true)
+                    else -> task.copy(isStarred = false)
+                }
+                listener.onTaskUpdated(updatedTask)
+            }
         }
+    }
+
+    interface TaskUpdatedListener {
+
+        fun onTaskUpdated(task: Task)
     }
 }
--- app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksFragment.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kraemericaindustries.getitdone.data.GetItDoneDatabase
+import com.kraemericaindustries.getitdone.data.Task
 import com.kraemericaindustries.getitdone.data.TaskDao
 import com.kraemericaindustries.getitdone.databinding.FragmentTasksBinding
 import kotlin.concurrent.thread
 
-class TasksFragment : Fragment() {
+class TasksFragment: Fragment(), TasksAdapter.TaskUpdatedListener {
 
     private lateinit var binding: FragmentTasksBinding
     private val taskDao: TaskDao by lazy {
@@ -35,8 +36,14 @@ class TasksFragment : Fragment() {
         thread {
             val tasks = taskDao.getAllTasks()
             requireActivity().runOnUiThread {
-                binding.recyclerView.adapter = TasksAdapter(tasks = tasks)
+                binding.recyclerView.adapter = TasksAdapter(tasks = tasks, listener = this)
             }
+        }
+    }
+
+    override fun onTaskUpdated(task: Task) {
+        thread {
+            taskDao.updateTask(task)
         }
     }
 }
