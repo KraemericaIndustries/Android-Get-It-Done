fef71cc347fa86995e6b69891fa28a3c84c601db
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-30 09:09:34 -0400

    Verified code working with DEV profile through lectures 265: "Refactor the Adapter (to improve performance)"

--- app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksAdapter.kt
+++ app/src/main/java/com/kraemericaindustries/getitdone/ui/tasks/TasksAdapter.kt
@@ -1,5 +1,6 @@
 package com.kraemericaindustries.getitdone.ui.tasks
 
+import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,8 +9,10 @@ import com.google.android.material.checkbox.MaterialCheckBox
 import com.kraemericaindustries.getitdone.data.Task
 import com.kraemericaindustries.getitdone.databinding.ItemTaskBinding
 
-class TasksAdapter(private val tasks: List<Task>, private val listener: TaskUpdatedListener) :
+class TasksAdapter(private val listener: TaskUpdatedListener) :
     RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
+
+    private var tasks: List<Task> = listOf()
 
     override fun getItemCount() = tasks.size
 
@@ -22,13 +25,19 @@ class TasksAdapter(private val tasks: List<Task>, private val listener: TaskUpda
         holder.bind(tasks[position])
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    fun setTasks(tasks: List<Task>) {
+        this.tasks = tasks
+        notifyDataSetChanged()
+    }
+
     inner class ViewHolder(private val binding: ItemTaskBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(task: Task) {
             binding.checkBox.isChecked = task.isComplete
             binding.toggleStar.isChecked = task.isStarred
-            if(task.isComplete) {
+            if (task.isComplete) {
                 binding.textViewTitle.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
                 binding.textViewDetails.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
             }
